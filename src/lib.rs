fe4282780747897d8c0ba02218d136d55574e1d0
Pavel Voronin <pavel.a.voronin@gmail.com>
2023-01-25 12:11:04 +0800

    add input feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
 use wasm_bindgen::prelude::*;
 
+extern crate console_error_panic_hook;
+
 const MEM_SIZE: usize = 30000;
 
 #[wasm_bindgen]
-pub fn interpret(program: &str) -> String {
+pub fn interpret(program: &str, input: &str) -> String {
+    console_error_panic_hook::set_once();
+
     let mut memory = [0; MEM_SIZE];
     let mut pointer: usize = 0;
 
     let mut position: usize = 0;
     let mut output = String::from("");
+
+    let mut input_position: usize = 0;
 
     while position < program.len() {
         let current_instruction = program.chars().nth(position).unwrap();
@@ -30,7 +36,10 @@ pub fn interpret(program: &str) -> String {
             '+' => memory[pointer] += 1,
             '-' => memory[pointer] -= 1,
             '.' => output.push(memory[pointer] as u8 as char),
-            ',' => memory[pointer] = read_byte(),
+            ',' => {
+                memory[pointer] = input.chars().nth(input_position).unwrap() as u8;
+                input_position += 1;
+            }
             '[' => {
                 if memory[pointer] == 0 {
                     position = matching_bracket(program, position);
@@ -76,10 +85,4 @@ fn matching_bracket(program: &str, mut position: usize) -> usize {
         }
     }
     position
-}
-
-fn read_byte() -> u8 {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    input.chars().nth(0).unwrap() as u8
 }
